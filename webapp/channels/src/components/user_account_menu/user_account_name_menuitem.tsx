51d532b0a596cbfadd54fb656cdb8539adefe685
Mohammad Shayestehfar <m.shayesteh301@gmail.com>
2025-11-03 16:52:08 +0200

    Refactor user account menu to comment out logout item and adjust height in content styles

--- webapp/channels/src/components/user_account_menu/user_account_name_menuitem.tsx
+++ webapp/channels/src/components/user_account_menu/user_account_name_menuitem.tsx
@@ -70,7 +70,7 @@ export default function UserAccountNameMenuItem({profilePicture, ...rest}: Props
                 />
             }
             labels={getLabel()}
-            onClick={handleClick}
+            // onClick={handleClick}
             aria-haspopup={true}
             {...rest}
         />
